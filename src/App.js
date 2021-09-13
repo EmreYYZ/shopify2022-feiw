726cdcca5133c30c4e05a33c3f6f011c7788e2f4
EmreYYZ <6238977+EmreYYZ@users.noreply.github.com>
2021-09-13 18:24:00 -0400

    Added Helmet Title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { Search } from "./components/Search";
 import { Layout } from "./components/layout/Layout";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <Layout>
+      <Helmet>
+        <title>🪐 Spacestagram</title>
+      </Helmet>
       <Search />
     </Layout>
   );
