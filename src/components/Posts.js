6fcca42798fc7378554bd81310263ba481d31b39
EmreYYZ <6238977+EmreYYZ@users.noreply.github.com>
2021-09-13 19:05:52 -0400

    Added fetch NASA data.

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export const Posts = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState([true]);
+
+  useEffect(() => {
+    let apiKey = "Z1MOBuhvpHaXD63Ap4LJWC3vJ4W6SOgfEPf3aMbw";
+    let apodURL = "https://api.nasa.gov/planetary/apod?api_key=";
+    fetch(`${apodURL}${apiKey}&count=9`)
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data);
+        setLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  if (loading) {
+    return (
+      <section>
+        <p>Loading the posts from NASA's Space Servers</p>
+      </section>
+    );
+  }
+
   return (
     <div>
       <p>Posts</p>
+      {posts.map((post) => (
+        <p>{post.title}</p>
+      ))}
     </div>
   );
 };
