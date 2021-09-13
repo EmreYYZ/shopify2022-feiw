5772dcb1a3270df4ca94745617ee27a83a07edc1
EmreYYZ <6238977+EmreYYZ@users.noreply.github.com>
2021-09-13 17:54:01 -0400

    Added search submit handler, and state

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Search = () => {
+  const [searchText, setSearchText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setSearchText("");
+  };
+
   return (
     <section>
-      <form action="">
+      <form action="" onSubmit={handleSubmit}>
         <p>Search Bar</p>
-        <input type="text" name="" id="" placeholder="" />
+        <label htmlFor="search"></label>
+        <input
+          type="text"
+          onChange={(e) => setSearchText(e.target.value)}
+          value={searchText}
+          name="search"
+          required
+          id="search"
+          placeholder="e.g Galaxy"
+        />
+        <button type="submit">Search</button>
       </form>
     </section>
   );
