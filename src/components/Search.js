7203035fcf2d8f6f0ab6b3822f5778a77eec2103
EmreYYZ <6238977+EmreYYZ@users.noreply.github.com>
2021-09-13 17:33:13 -0400

    Created Search component

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Search = () => {
+  return (
+    <section>
+      <form action="">
+        <p>Search Bar</p>
+        <input type="text" name="" id="" placeholder="" />
+      </form>
+    </section>
+  );
+};
