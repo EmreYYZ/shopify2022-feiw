e551a6d21e70ebb320ab134c1b3a15a3a7689086
EmreYYZ <6238977+EmreYYZ@users.noreply.github.com>
2021-09-13 22:08:03 -0400

    Removed Search

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { Search } from "./components/Search";
 import { Layout } from "./components/layout/Layout";
 import { Helmet } from "react-helmet";
 import { Posts } from "./components/Posts";
