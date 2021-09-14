d81fcf73ac13ef2ac5ea150fb41cd5621ae509e2
EmreYYZ <6238977+EmreYYZ@users.noreply.github.com>
2021-09-13 20:19:39 -0400

    Created the basic post layout / no styling, will improve

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,12 +1,23 @@
 import React from "react";
 import { Avatar } from "./Avatar";
+import { PostDescription } from "./PostDescription";
+import { PostImage } from "./PostImage";
+import { PostVideo } from "./PostVideo";
 
-export const PostCard = ({ title, description, tags, date, image }) => {
+export const PostCard = ({ title, author, description, date, mediaType, imageSD, imageHD }) => {
+  if (author === undefined) {
+    author = "A random space scientist.";
+  }
+
   return (
-    <div>
-      <p>kasjdk</p>
+    <article>
+      <h2>{title}</h2>
+      <p>{date}</p>
+      <p>{author}</p>
+      {mediaType === "image" ? <PostImage imageHD={imageHD} title={title} imageSD={imageSD} /> : <PostVideo videoURL={imageSD} title={title} />}
 
-      <Avatar date={date} />
-    </div>
+      <PostDescription description={description} />
+      <Avatar author={author} date={date} />
+    </article>
   );
 };
