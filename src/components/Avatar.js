dc8619bca1a97911f5958110c09fdf6265669e23
EmreYYZ <6238977+EmreYYZ@users.noreply.github.com>
2021-09-13 20:19:02 -0400

    Added avatars styling conditional avatar generation

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-export const Avatar = ({ date }) => {
+export const Avatar = ({ author, date }) => {
   return (
     <picture>
-      <img src={`https://robohash.org/${date}.png?set=set2`} alt="Avatar of the user." srcset="" />
+      {author === "A random scientist." ? (
+        <img
+          className="rounded-full w-28 h-28 border-2 border-gray-900 bg-pink-200"
+          src={`https://robohash.org/${author}.png?set=set2`}
+          alt="Avatar of the user."
+        />
+      ) : (
+        <img
+          className="rounded-full w-28 h-28 border-2 border-gray-900 bg-pink-200"
+          src={`https://robohash.org/${date}.png?set=set2`}
+          alt="Avatar of the user."
+        />
+      )}
     </picture>
   );
 };
