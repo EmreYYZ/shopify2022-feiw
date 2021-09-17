1b40870df4ce701e413f755e592ca086256ea835
EmreYYZ <6238977+EmreYYZ@users.noreply.github.com>
2021-09-16 22:59:44 -0400

    Added single post page for shared posts

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Helmet } from "react-helmet";
 import { Posts } from "./components/Posts";
 import { useState, useEffect } from "react";
 import { LikedPosts } from "./components/LikedPosts";
+import { SinglePost } from "./components/SinglePost";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
@@ -25,8 +26,11 @@ function App() {
           <Route path="/" exact>
             <Posts likedPosts={likedPosts} setLikedPosts={setLikedPosts} />
           </Route>
-          <Route path="/likes">
+          <Route path="/likes" exact>
             <LikedPosts likedPosts={likedPosts} setLikedPosts={setLikedPosts} />
+          </Route>
+          <Route path="/:date">
+            <SinglePost likedPosts={likedPosts} setLikedPosts={setLikedPosts} />
           </Route>
         </Switch>
       </Layout>
---
+++ src/components/SinglePost.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import { PostCard } from "./PostCard";
+import { useParams } from "react-router";
+
+export const SinglePost = ({ likedPosts, setLikedPosts }) => {
+  const [post, setPost] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  let { date } = useParams();
+  useEffect(() => {
+    let apiKey = "Z1MOBuhvpHaXD63Ap4LJWC3vJ4W6SOgfEPf3aMbw";
+    let apodURL = "https://api.nasa.gov/planetary/apod?api_key=";
+    fetch(`${apodURL}${apiKey}&date=${date}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setPost(data);
+        console.log(data);
+        setLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  if (loading) {
+    return (
+      <section>
+        <p>Loading the posts from NASA's Space Servers, this can take a bit of time.</p>
+      </section>
+    );
+  }
+
+  return (
+    <>
+      <PostCard
+        likedPosts={likedPosts}
+        setLikedPosts={setLikedPosts}
+        title={post.title}
+        date={post.date}
+        description={post.explanation}
+        mediaType={post.media_type}
+        imageSD={post.url}
+        imageHD={post.hdurl}
+        author={post.copyright}
+      />
+    </>
+  );
+};
