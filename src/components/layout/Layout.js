2addfdd6f3f80a0314368630c94028a4f81cc012
EmreYYZ <6238977+EmreYYZ@users.noreply.github.com>
2021-09-20 12:29:14 -0400

    Build and deploy

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -4,7 +4,7 @@ import { Footer } from "./Footer";
 
 export const Layout = ({ children }) => {
   return (
-    <div className="max-w-lg  mx-3 sm:m-auto sm:my-4 my-8">
+    <div className="max-w-lg mx-3 sm:m-auto sm:my-4 my-8">
       <Header />
       {children}
       <Footer />
